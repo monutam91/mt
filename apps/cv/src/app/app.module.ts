44f63103d36f652be23e020f5f8670701186ffa7
Mónus Tamás <tamas.monus@nitrowise.com>
2019-04-11 12:14:04 +0200

    Desktop design ready, need to align it with the design samples

--- apps/cv/src/app/app.module.ts
+++ apps/cv/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NxModule } from '@nrwl/nx';
 
 import { AppComponent } from './app.component';
 import { MtInfoItemModule } from './components/public_api';
+import { ScrollWithBodyDirective } from './directives/public_api';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, ScrollWithBodyDirective],
     imports: [BrowserModule, NxModule.forRoot(), MtInfoItemModule],
     providers: [],
     bootstrap: [AppComponent]
---
+++ apps/cv/src/app/directives/scroll-with-body/scroll-with-body.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScrollWithBodyDirective } from './scroll-with-body.directive';
+
+describe('ScrollWithBodyDirective', () => {
+    it('should create an instance', () => {
+        const directive = new ScrollWithBodyDirective();
+        expect(directive).toBeTruthy();
+    });
+});
