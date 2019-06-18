ca45972c5a03845631304aace3be094db1f39222
Mónus Tamás <tamas.monus@nitrowise.com>
2019-06-18 11:25:39 +0200

    Added hu i18n for the application

--- apps/cv/src/app/app.module.ts
+++ apps/cv/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NxModule } from '@nrwl/angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { InfosModule, MtLabelValueModule } from './components/public_api';
 import { ScrollWithBodyDirective } from './directives/public_api';
 
+export function translateLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
+
 @NgModule({
     declarations: [AppComponent, ScrollWithBodyDirective],
-    imports: [BrowserModule, NxModule.forRoot(), InfosModule, MtLabelValueModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        InfosModule,
+        MtLabelValueModule,
+        TranslateModule.forRoot({
+            loader: { provide: TranslateLoader, useFactory: translateLoaderFactory, deps: [HttpClient] }
+        })
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
