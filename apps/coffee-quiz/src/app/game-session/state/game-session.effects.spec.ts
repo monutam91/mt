3f64128680a71fbc646cc5fb41befe1751feefec
Mónus tamás <monutam91@gmail.com>
2021-04-19 12:58:10 +0200

    Game session effects are done with tests, no error feedback for the user yet

--- apps/coffee-quiz/src/app/game-session/state/game-session.effects.spec.ts
+++ apps/coffee-quiz/src/app/game-session/state/game-session.effects.spec.ts
@@ -1,27 +1,71 @@
 import { TestBed } from '@angular/core/testing';
-
-import { Observable } from 'rxjs';
-
 import { provideMockActions } from '@ngrx/effects/testing';
+import { hot } from '@nrwl/angular/testing';
 
-import { NxModule } from '@nrwl/angular';
+import { of, throwError } from 'rxjs';
+
+import { SessionService } from '../services/session.service';
+import { createSession, createSessionFailed, createSessionSuccess } from './game-session.actions';
 
 import { GameSessionEffects } from './game-session.effects';
 
 describe('GameSessionEffects', () => {
-    let actions: Observable<any>;
+    const fakeToken = 'FAKE_TOKEN';
+    let actions;
     let effects: GameSessionEffects;
+    let mockSessionService: SessionService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [NxModule.forRoot()],
-            providers: [GameSessionEffects, provideMockActions(() => actions)],
+            providers: [
+                GameSessionEffects,
+                provideMockActions(() => actions),
+                {
+                    provide: SessionService,
+                    useValue: {
+                        requestSessionToken: jest.fn(() => of(fakeToken)),
+                    },
+                },
+            ],
         });
 
         effects = TestBed.inject(GameSessionEffects);
+        mockSessionService = TestBed.inject(SessionService);
     });
 
     it('should create', () => {
-        expect(effects).toBeTruthy()
-    })
+        expect(effects).toBeTruthy();
+    });
+
+    describe('Given a createSession action dispatched', () => {
+        describe('When the requestSession succeeds', () => {
+            beforeEach(() => {
+                mockSessionService.requestSessionToken = jest.fn(() => of(fakeToken));
+            });
+
+            it('Then the requestSession method should be called And createSessionSuccess should be dispatched', () => {
+                actions = hot('-a', { a: createSession({ name: 'Test Name' }) });
+
+                const expected = hot('-a', { a: createSessionSuccess({ name: 'Test Name', token: fakeToken }) });
+
+                expect(effects.createSession$).toBeObservable(expected);
+                expect(mockSessionService.requestSessionToken).toHaveBeenCalled();
+            });
+        });
+
+        describe('When the requestSession failes', () => {
+            beforeEach(() => {
+                mockSessionService.requestSessionToken = jest.fn(() => throwError(new Error('Fake Error')));
+            });
+
+            it('Then the createSessionFailed should be dispatched', () => {
+                actions = hot('-a', { a: createSession({ name: 'Test Name' }) });
+
+                const expected = hot('-a', { a: createSessionFailed({ reason: new Error('Fake Error') }) });
+
+                expect(effects.createSession$).toBeObservable(expected);
+                expect(mockSessionService.requestSessionToken).toHaveBeenCalled();
+            });
+        });
+    });
 });
--- apps/coffee-quiz/src/app/game-session/state/game-session.effects.ts
+++ apps/coffee-quiz/src/app/game-session/state/game-session.effects.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+
+import { SessionService } from '../services/session.service';
+import { createSession, createSessionFailed, createSessionSuccess } from './game-session.actions';
 
 @Injectable()
 export class GameSessionEffects {
-    constructor(private actions$: Actions) {}
+    public createSession$: Observable<Action> = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(createSession),
+            switchMap((action) => {
+                return this.session.requestSessionToken().pipe(
+                    map((token) => createSessionSuccess({ name: action.name, token })),
+                    catchError((error) => of(createSessionFailed({ reason: error })))
+                );
+            })
+        );
+    });
+
+    constructor(private actions$: Actions, private session: SessionService) {}
 }
